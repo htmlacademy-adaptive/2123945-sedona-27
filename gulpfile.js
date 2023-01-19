fa84cad2da1941e973741e0727fa5bdf931eb17e
Алена <arslanbekova@gmail.com>
2023-01-19 18:35:26 +0300

    завершение сборки

--- gulpfile.js
+++ gulpfile.js
@@ -11,7 +11,6 @@ import svgo from 'gulp-svgmin';
 import svgstore from 'gulp-svgstore';
 import del from 'del';
 import browser from 'browser-sync';
-import htmlmin from 'gulp-htmlmin';
 
 // Styles
 
@@ -30,9 +29,8 @@ csso()
 
 // HTML
 
-export const html = () => {
+const html = () => {
 return gulp.src('source/*.html')
-.pipe(htmlmin({ collapseWhitespace: true }))
 .pipe(gulp.dest('build'));
 }
 
