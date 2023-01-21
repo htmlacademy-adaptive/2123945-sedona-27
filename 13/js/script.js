ebe77fe243764e9d945f9f0aa2278e700f71e35d
Keks <keks@htmlacademy.ru>
2023-01-22 02:28:06 +0300

    :heavy_check_mark: Сборка #13

---
+++ 13/js/script.js
@@ -0,0 +1,41 @@
+let navMain = document.querySelector('.main-nav');
+let navToggle = document.querySelector('.main-nav__toggle');
+
+navMain.classList.remove('main-nav--nojs');
+
+navToggle.addEventListener('click', function () {
+  if (navMain.classList.contains('main-nav--closed')) {
+    navMain.classList.remove('main-nav--closed');
+    navMain.classList.add('main-nav--opened');
+  } else {
+    navMain.classList.add('main-nav--closed');
+    navMain.classList.remove('main-nav--opened');
+  }
+});
+
+let submitButton = document.querySelector('.submit-review');
+//let modalContainerSuccess = document.querySelector('.modal-container--success');
+//let closeButtonSuccess = document.querySelector('.button--success');
+
+//navMain.classList.remove('main-nav--nojs');
+
+//submitButton.addEventListener('click', function () {
+//  modalContainerSuccess.classList.add('modal-container--open');
+//});
+
+//closeButtonSuccess.addEventListener('click', function () {
+//  modalContainerSuccess.classList.remove('modal-container--open');
+//});
+
+let modalContainerFailure = document.querySelector('.modal-container--failure');
+let closeButtonFailure = document.querySelector('.button--failure');
+
+navMain.classList.remove('main-nav--nojs');
+
+submitButton.addEventListener('click', function () {
+  modalContainerFailure.classList.add('modal-container--open');
+});
+
+closeButtonFailure.addEventListener('click', function () {
+  modalContainerFailure.classList.remove('modal-container--open');
+});
