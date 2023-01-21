57f86eab5a1a4e886a3635c5b98e5f16022051fa
Алена <arslanbekova@gmail.com>
2023-01-22 02:19:13 +0300

    модальное fail -планш деск

--- source/js/script.js
+++ source/js/script.js
@@ -14,28 +14,28 @@ navToggle.addEventListener('click', function () {
 });
 
 let submitButton = document.querySelector('.submit-review');
-let modalContainerSuccess = document.querySelector('.modal-container--success');
-let closeButtonSuccess = document.querySelector('.button--success');
+//let modalContainerSuccess = document.querySelector('.modal-container--success');
+//let closeButtonSuccess = document.querySelector('.button--success');
+
+//navMain.classList.remove('main-nav--nojs');
+
+//submitButton.addEventListener('click', function () {
+//  modalContainerSuccess.classList.add('modal-container--open');
+//});
+
+//closeButtonSuccess.addEventListener('click', function () {
+//  modalContainerSuccess.classList.remove('modal-container--open');
+//});
+
+let modalContainerFailure = document.querySelector('.modal-container--failure');
+let closeButtonFailure = document.querySelector('.button--failure');
 
 navMain.classList.remove('main-nav--nojs');
 
 submitButton.addEventListener('click', function () {
-  modalContainerSuccess.classList.add('modal-container--open');
+  modalContainerFailure.classList.add('modal-container--open');
 });
 
-closeButtonSuccess.addEventListener('click', function () {
-  modalContainerSuccess.classList.remove('modal-container--open');
+closeButtonFailure.addEventListener('click', function () {
+  modalContainerFailure.classList.remove('modal-container--open');
 });
-
-//let modalContainerFailure = document.querySelector('.modal-container--failure');
-//let closeButtonFailure = document.querySelector('.button--failure');
-
-////navMain.classList.remove('main-nav--nojs');
-
-//submitButton.addEventListener('click', function () {
-//  modalContainerFailure.classList.add('modal-container--open');
-//});
-
-//closeButtonFailure.addEventListener('click', function () {
-//  modalContainerFailure.classList.remove('modal-container--open');
-//});
